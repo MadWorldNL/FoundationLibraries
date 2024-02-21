11a8537cc93b27b81e25407ceaecaa78dc0ae754
Oscar Veldman <oveldman@gmail.com>
2024-02-21 18:02:45 +0100

    Add connection string builder

--- sources/Common.AspNetCore/WebApplicationBuilderExtensions.cs
+++ sources/Common.AspNetCore/WebApplicationBuilderExtensions.cs
@@ -1,5 +1,6 @@
 using MadWorldNL.Common.Time;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MadWorldNL.Common.AspNetCore;
@@ -9,5 +10,12 @@ public static class WebApplicationBuilderExtensions
     public static void AddCommonMadWorldNL(this WebApplicationBuilder app)
     {
         app.Services.AddSingleton<IDateTimeProvider, DateTimeProvider>();
+    }
+    
+    public static string BuildConnectionString(this WebApplicationBuilder builder, string connectionStringName)
+    {
+        var connectionString = builder.Configuration.GetValue<string>($"DbContext:{connectionStringName}")!;
+        var password = builder.Configuration.GetValue<string>("DbContext:Password")!;
+        return connectionString.Replace("{password}", password);
     }
 }
